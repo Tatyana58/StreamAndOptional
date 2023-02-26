8c12548a7d237c83d144304d93ae2739b50bc44e
Tatyana58 <tatbir@rambler.ru>
2023-02-26 19:25:22 +0300

    Начало Задание №1 Заданние №2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 import java.util.function.BiConsumer;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -14,7 +17,7 @@ public class Main {
         BiConsumer<Person, Person> biConsumer = (min, max) -> System.out.println(min.toString() + max.toString());
         Comparator<Person> personComparator=Comparator.comparing(Person::getAge).thenComparing(Person::getName);
         findMinMax(personStream,personComparator,biConsumer);
-
+        findEvenNum(List.of(5,768,16,4,9,13,11,17,38,5));
     }
     public static<T> void findMinMax(
             Stream<? extends T> stream,
@@ -28,7 +31,18 @@ public class Main {
             minMaxConsumer.accept(List.get(0), List.get(List.size() - 1));
         }
     }
+/*Задание 2
+Реализуйте метод, который принимает на вход список целых чисел, определяет в списке количество четных чисел и выводит их в консоль.
+Решите задание именно с применением Stream API.
+Критерии проверки
+Задача решена корректно.
+Соблюден кодстайл.
+Правильно реализован предикат.*/
+    public static void findEvenNum(List<Integer> list) {
+       Stream<Integer> stream = list.stream();
+       Predicate<Integer> predicate = (s) -> (s % 2) == 0;
+       Stream<Integer> integerStream = stream.filter(predicate);
+        System.out.println("Количество четных чисел - "+integerStream.count());
 
-
-
+    }
 }
