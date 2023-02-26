f04af44002c1b8c5dd3dc5973991bbff5d80fc8f
Tatyana58 <tatbir@rambler.ru>
2023-02-26 19:28:06 +0300

    Начало Задание №1 Заданние №2 Откорректировала.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Objects;
 import java.util.function.BiConsumer;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -15,9 +13,9 @@ public class Main {
                     new Person("Василий",71),
                     new Person("Валентина",42));
         BiConsumer<Person, Person> biConsumer = (min, max) -> System.out.println(min.toString() + max.toString());
-        Comparator<Person> personComparator=Comparator.comparing(Person::getAge).thenComparing(Person::getName);
-        findMinMax(personStream,personComparator,biConsumer);
-        findEvenNum(List.of(5,768,16,4,9,13,11,17,38,5));
+        Comparator<Person> personComparator = Comparator.comparing(Person::getAge).thenComparing(Person::getName);
+        findMinMax(personStream, personComparator, biConsumer);
+        findEvenNum(List.of(5, 768, 16, 4, 9, 13, 11, 17, 38, 5));
     }
     public static<T> void findMinMax(
             Stream<? extends T> stream,
@@ -39,10 +37,12 @@ public class Main {
 Соблюден кодстайл.
 Правильно реализован предикат.*/
     public static void findEvenNum(List<Integer> list) {
-       Stream<Integer> stream = list.stream();
-       Predicate<Integer> predicate = (s) -> (s % 2) == 0;
-       Stream<Integer> integerStream = stream.filter(predicate);
-        System.out.println("Количество четных чисел - "+integerStream.count());
+        Stream<Integer> stream = list.stream();
+        Predicate<Integer> predicate = (s) -> (s % 2) == 0;
+        Stream<Integer> integerStream = stream.filter(predicate);
+        System.out.println("Количество четных чисел - " + integerStream.count());
+
 
     }
 }
+
