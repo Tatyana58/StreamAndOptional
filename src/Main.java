f23b1e89a9f79c509760866cd4510956f5979835
Tatyana58 <tatbir@rambler.ru>
2023-02-26 19:10:03 +0300

    Начало Задание №1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Stream<Person> personStream = Stream.of(new Person("Иван",17),
+                    new Person("Сергей",61),
+                    new Person("Василий",71),
+                    new Person("Валентина",42));
+        BiConsumer<Person, Person> biConsumer = (min, max) -> System.out.println(min.toString() + max.toString());
+        Comparator<Person> personComparator=Comparator.comparing(Person::getAge).thenComparing(Person::getName);
+        findMinMax(personStream,personComparator,biConsumer);
+
     }
+    public static<T> void findMinMax(
+            Stream<? extends T> stream,
+            Comparator<? super T> order,
+            BiConsumer<? super T, ? super T> minMaxConsumer) {
+        List<T> List = stream.collect(Collectors.toList());
+        if (List.isEmpty()) {
+            minMaxConsumer.accept(null,null);
+        }else {
+            List.sort(order);
+            minMaxConsumer.accept(List.get(0), List.get(List.size() - 1));
+        }
+    }
+
+
+
 }
